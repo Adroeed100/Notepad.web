2d6e6920e7c5c1a6cc39baee3d2e602062f3bfb3
Adroeed100 <muhammedthanseef7@gmail.com>
2025-06-09 20:03:57 +0530

    Edit feature added

--- index.js
+++ index.js
@@ -25,13 +25,22 @@ app.get('/file/:filename', (req, res) => {
     })
 })
 
+app.get('/edit/:filename', (req, res) => {    
+    res.render('edit',  {filename: req.params.filename});
+})
+
+app.post('/edit', (req, res) => {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err)=> {
+        res.redirect('/');
+    })
+})
+
 app.post('/create', (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) =>{
         if (err) throw err;
         res.redirect('/');
     });
 })
-
 
 app.listen(3000, () => {
     console.log('Server running at http://localhost:3000');
